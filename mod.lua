b558c4304ffcb69ddff42344c3e5437663f14fd6
Florian Thiel <flth@flth.de>
2023-12-20 14:36:09 +0100

    initial von jay

---
+++ mod.lua
@@ -0,0 +1,109 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+		        name = "jay_",
+		        role = "CREATOR",
+		    },
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+
+		},
+		tags = { "europe", "container", "Intermodal", "taschenwagen", "wagon", },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	  
+		params = {
+			{
+				key = "taschwagenT1T3fake",
+				name = _("Fake_taschwagenT1T3_wagen"),
+				values = { "No", "Yes", },
+				defaultIndex = 0,
+			},	
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	local params = modParams[getCurrentModId()]
+
+        local hidden = {
+			["db06_fake.mdl"] = true,
+			["db73_fake.mdl"] = true,
+			["db80_fake.mdl"] = true,
+			["db80_pritsche_fake.mdl"] = true,
+			["db94_fake.mdl"] = true,
+			["db94_pritsche_fake.mdl"] = true,
+			["fs_fake.mdl"] = true,
+			["hupac06_fake.mdl"] = true,
+			["hupac73_fake.mdl"] = true,
+			["hupac90_fake.mdl"] = true,
+			["hupac90_pritsche_fake.mdl"] = true,
+			["novatrans_fake.mdl"] = true,
+			["novatrans2_fake.mdl"] = true,
+			["novatrans06_fake.mdl"] = true,
+			["novatrans80_fake.mdl"] = true,
+			["oebb_fake.mdl"] = true,
+			["oebb_pritsche_fake.mdl"] = true,
+			["oebb2_fake.mdl"] = true,
+			["sati_fake.mdl"] = true,
+			["segi_fake.mdl"] = true,
+			["db80_wohlfahrt_fake.mdl"] = true,
+			["db94_lauritzen_fake.mdl"] = true,
+			["fs_sae_fake.mdl"] = true,
+			["hupac73_wetram_fake.mdl"] = true,
+			["hupac90_hangartner_fake.mdl"] = true,
+			["oebb_lkwWalter_fake.mdl"] = true,
+			["segi_danzas_fake.mdl"] = true,
+			
+			["aae_fake.mdl"] = true,
+			["aae_danzas_fake.mdl"] = true,
+			["db_fake.mdl"] = true,
+			["db2_fake.mdl"] = true,
+			["dsb_fake.mdl"] = true,
+			["dsb_sattel_fake.mdl"] = true,
+			["dsb_dsbGods_fake.mdl"] = true,
+			["dsb_norsk_fake.mdl"] = true,
+			["hupac_fake.mdl"] = true,
+			["hupac_pritsche_fake.mdl"] = true,
+			["hupac_hangartner_fake.mdl"] = true,
+			["hupac06_fake.mdl"] = true,
+			["kombiwaggon_fake.mdl"] = true,
+			["kombiwaggon_pritsche_fake.mdl"] = true,
+			["oebb_fake.mdl"] = true,
+			["oebb06_fake.mdl"] = true,
+			["oebb_pritsche_fake.mdl"] = true,
+			["oebb_walter_fake.mdl"] = true,
+			["sj_fake.mdl"] = true,
+			["sj_sattel_fake.mdl"] = true,
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/t1_([^/]*.mdl)')			
+						or fileName:match('/t3_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["taschwagenT1T3fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+		
+		addModifier( "loadModel", metadataHandler )
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,55 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Pocket Wagons T1/T3",
+		["mod_desc"] = "Pocket wagons are specialized rail vehicles that have been developed for combined transport since 1973. They enable the transportation of semi-trailers, containers and swap bodies. The wagons have a low-lying loading area with folding support pins for securing containers and swap bodies as well as a fifth wheel coupling for supporting semi-trailers. These universal wagons adapt to the changing requirements of trailer and container technology every ten years or so, resulting in a wide range of design variants.",
+
+        ["t1_wagen_desc"] = "The T1 (a) pocket wagon type was coordinated by the UIRR and developed by the Deutsche Bundesbahn and the SNCF to transport semi-trailers, swap bodies and containers. The first wagons were put into service in 1973, and a total of around 1500 wagons were used until the end of the 1990s. DB Schenker Rail AG carried out an upgrade (T1b) with larger wheelsets and increased bogie pivot spacing. DB Cargo AG used parts from decommissioned wagons for new models. A project on the future of combined transport emphasizes the need to phase out the remaining T1a wagons in Europe.",
+        ["t3_wagen_desc"] = "The T1a pocket wagon type was replaced by the T3 from 1984 due to its low payload. At 18,340 mm, the T3 is almost two meters longer than the T1 and has a load limit of 59 t, which was later increased to 69 t. The T3 enables the transportation of heavy semi-trailers, but not mega trailers. Compared to the T1, the tare weight increased by five tons to 21 tons. In the 1990s, MSV Studénka, Talbot and Waggonbau Niesky produced the T3. The inefficient use of the loading area for container transportation was a problem. At the end of 2018, 160 of the original 170 Sdgmns743s were still registered with DB AG. Austrian Rail Cargo has 200 T3 wagons of type Sdgmnss4587, which are approved for 120 km/h in terms of braking, compared to the 100 km/h of DB Cargo's Sdgmns743.",
+		
+				["height"] = "Height",
+		["height_tooltip"] = "Configure height",
+		["jrm_param_offsetx_tooltip"] = "Configure offset",
+		["jrm_param_type_tooltip"] = "Type",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+		["Fake_taschwagenT1T3_wagen"] = "Activate Fake versions?",
+		["t3_wagen_menu"] = "Pocket Wagons T1 & T3",
+		["t3_wagen_menu_fake"] = "Pocket Wagons T1 & T3 FAKE",
+		["assetVariante_1"] = "Empty",
+		["assetVariante_2"] = "Trailer",
+		["assetVariante_3"] = "Swap Body",
+		["assetVariante_4"] = "Container 20ft",
+		["assetVariante_5"] = "Container 40ft",
+
+	},
+	de = {
+
+		["mod_name"] = "Taschenwagen T1/T3",
+		["mod_desc"] = "Taschenwagen sind spezialisierte Schienenfahrzeuge, die seit 1973 für den kombinierten Verkehr entwickelt wurden. Sie ermöglichen den Transport von Sattelanhängern, Containern und Wechselbehältern. Die Wagen verfügen über eine tief liegende Ladefläche mit klappbaren Aufsetzzapfen für die Fixierung von Containern und Wechselbehältern sowie einer Sattelkupplung für die Stützung von Sattelanhängern. Diese Universalwagen passen sich den sich wandelnden Anforderungen der Trailer- und Container-Technologie alle etwa zehn Jahre an, was zu einer Vielfalt von Bauvarianten führt.",
+
+        ["t1_wagen_desc"] = "Die Taschenwagen-Bauart T1 (a) wurde von der UIRR koordiniert und von der Deutschen Bundesbahn sowie der SNCF entwickelt, um Sattelanhänger, Wechselbehälter und Container zu transportieren. Die ersten Wagen wurden 1973 in Betrieb genommen, insgesamt wurden etwa 1500 Wagen bis Ende der 1990er Jahre eingesetzt. Die DB Schenker Rail AG führte eine Aufwertung (T1b) mit größeren Radsätzen und erhöhtem Drehzapfenabstand durch. Die DB Cargo AG verwendete Teile ausgemusterter Wagen für neue Modelle. Ein Projekt zur Zukunft des kombinierten Verkehrs betont die Notwendigkeit der Ausmusterung der verbleibenden T1a-Wagen in Europa.",
+        ["t3_wagen_desc"] = "Die Taschenwagen-Bauart T1a wurde aufgrund ihrer geringen Nutzlast ab 1984 durch die T3 ersetzt. Der T3 ist mit 18.340 mm fast zwei Meter länger als der T1 und hat eine Lastgrenze von 59 t, die später auf 69 t erhöht wurde. Der T3 ermöglicht den Transport schwerer Sattelauflieger, aber keine Megatrailer. Das Eigengewicht stieg im Vergleich zum T1 um fünf Tonnen auf 21 t. In den 1990er Jahren produzierten MSV Studénka, Talbot und Waggonbau Niesky den T3. Die ineffiziente Nutzung der Ladefläche bei Containertransporten war ein Problem. Ende 2018 waren von den ursprünglich 170 Sdgmns743 noch 160 bei der DB AG registriert. Die österreichische Rail Cargo hat 200 T3-Wagen vom Typ Sdgmnss4587, die bremstechnisch für 120 km/h zugelassen sind, im Vergleich zu den 100 km/h des Sdgmns743 der DB Cargo.",
+		
+		["height"] = "Höhe",
+		["height_tooltip"] = "Höhe einstellen",
+		["jrm_param_offsetx_tooltip"] = "Versatz einstellen",
+		["jrm_param_type_tooltip"] = "Typ",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+		["Fake_taschwagenT1T3_wagen"] = "Fakeversionen aktivieren?",
+		["t3_wagen_menu"] = "Taschenwagen T1 & T3",
+		["t3_wagen_menu_fake"] = "Taschenwagen T1 & T3 FAKE",
+		["assetVariante_1"] = "Leer",
+		["assetVariante_2"] = "Sattelauflieger",
+		["assetVariante_3"] = "Wechselpritschen",
+		["assetVariante_4"] = "Container 20ft",
+		["assetVariante_5"] = "Container 40ft",
+	},
+}
+end
